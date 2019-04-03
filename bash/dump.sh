a3c4408cab4cc90f17ee526541000bcd589f3da7
Marius Zander <zandermarius1@gmail.com>
2019-04-03 17:04:33 +0200

    Update dump.sh

--- bash/dump.sh
+++ bash/dump.sh
@@ -4,6 +4,7 @@ DUMP="../captures/" # Where to store the captured data.
 DEVICE=$(printenv 'MONITOR_DEVICE') # Get the device from the enviorment.
 DATE=$(date +"%F")
 
+./channelhopper.sh 5 &
 tcpdump -i $DEVICE -w $DUMP"wireless_$DATE.cap" -n -e -s 256 type mgt subtype probe-resp or subtype probe-req
 
 
