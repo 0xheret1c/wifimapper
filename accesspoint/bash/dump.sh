b49468adab1f389416d11a6a45300a8bd1743d2a
Marius Zander <zandermarius1@gmail.com>
2019-04-12 12:19:52 +0200

    Added DB stuff

--- accesspoint/bash/dump.sh
+++ accesspoint/bash/dump.sh
@@ -4,6 +4,7 @@ DUMP="../captures/" # Where to store the captured data.
 DEVICE=$(printenv 'MONITOR_DEVICE') # Get the device from the enviorment.
 DATE=$(date +"%F")
 
-tcpdump -i $DEVICE -vv -n -e -s 256 type mgt subtype probe-req #| ../python/to_database.py
+tcpdump -i $DEVICE -vv -n -e -s 256 type mgt subtype probe-req | ../python/to_database.py
+
 
 
--- accesspoint/python/to_database.py
+++ accesspoint/python/to_database.py
@@ -1,12 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
+import mysql.connector as mariadb
 import sys
 import time
 LOCATION = "LOCATION_A"
-DEVICE_NUMBER = "1S"
+AP_NUMBER = "0"
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = "1337"
+SERVER_USER = "mariadb"
+SERVER_PASS = "root"
+
+
+
+def connect():
+    return mariadb.connect(host=SERVER_IP,port=SERVER_PORT,user=SERVER_USER, password=SERVER_PASS, database='data')
+
+connection = connect()
 
 def sendToDB(data):
-    print(DEVICE_NUMBER + " " + LOCATION + " " + data, end ='')
+    splt = data.split()
+    if(len(splt) <= 0):
+        return
+    
+    time = splt[0]
+    strength = splt[6]
+    source = splt[13].replace("SA:","")
+    print("Time: " + time)
+    print("Strength: " + strength)
+    print("Source: " + source)
+    print(" ")
+
+
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO captures (location,ap,time,strength,source) VALUES (%s,%s)", (LOCATION, AP_NUMBER,time,strength,source))
+
 
 while True:
     data = sys.stdin.readline()
