9f69563e6e9742382ab5b80d5643001b55485d07
Marius Zander <zandermarius1@gmail.com>
2019-06-19 22:11:37 +0200

    Added bluetooth stuff

--- dump.py
+++ dump.py
@@ -1,13 +1,39 @@
-
 #!/usr/bin/python3
 import os
 import json
+import sys
 
+# Load config to get device settings.
 with open("./conf.json", 'r') as f:
     config = json.load(f)
 
-DEV = config["ap"]["network_monitor_dev"]
+WIFI_DEV = config["ap"]["network_monitor_dev"]
+BT_DEV = config["ap"]["bluetooth_dev"]
 
-os.system("tcpdump -i " + DEV + " -vv -n -e -s 256 type mgt subtype probe-req | ../to_database.py")
+
+def sniffWifi():
+        command = "tcpdump -i " + WIFI_DEV + " -vv -n -e -s 256 type mgt subtype probe-req | ../to_database.py"
+        os.system(command)
+
+def sniffBluetooth():
+        command = "tcpdump -i " + BT_DEV  #| ../to_database.py"
+        os.system(command)
+
+
+argv = sys.argv
+argc = len(argv)
+
+os.system("./printlogo.py")
+
+# Dump depending on arg. Default is bluetooth
+if(argc > 1):
+    print("Too many arguments!")
+    exit()
+elif(argc == 1):
+    if(argv[0] == "w" or argv[0] == "wifi"):
+        sniffWifi()
+    else:
+        sniffBluetooth()
+
 
 
